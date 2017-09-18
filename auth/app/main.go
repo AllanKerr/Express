2448efdf45ab73aabc32a73c26da103215df4bbc
Allan Kerr <personal.allankerr@gmail.com>
2017-09-18 13:25:46 -0600

    Refactored the project to have core files in the core package rather than main to allow sharing between projects.

--- auth/app/main.go
+++ auth/app/main.go
@@ -2,14 +2,17 @@ package main
 
 import (
 	"github.com/sirupsen/logrus"
+	"services/oauth2"
+	"services/core"
 )
 
 func main() {
 
-	ds, err := NewCQLDatastore("cassandra-0.cassandra:9042", "default")
+	ds, err := core.NewCQLDatastore("cassandra-0.cassandra:9042", "default")
 	if err != nil {
 		logrus.WithError(err).Fatal("Failed to create CQL datastore.")
 	}
-	app := NewApp(ds)
+	app := core.NewApp(ds)
+	oauth2.NewHTTPController(app)
 	app.Start(8080)
 }
--- auth/core/app.go
+++ auth/core/app.go
@@ -1,4 +1,4 @@
-package main
+package core
 
 import (
 	"github.com/gorilla/mux"
@@ -12,6 +12,10 @@ type App struct {
 	datastore Datastore
 }
 
+func (app *App)GetDatastore() Datastore {
+	return app.datastore
+}
+
 func NewApp(ds Datastore) *App {
 	if ds == nil {
 		logrus.Fatal("Attempted to create new app with nil datastore.");
@@ -20,6 +24,15 @@ func NewApp(ds Datastore) *App {
 	app.router = mux.NewRouter()
 	app.datastore = ds
 	return app
+}
+
+func (app *App)AddEndpoint(path string, internal bool, f func(http.ResponseWriter, *http.Request)) *mux.Route {
+
+	route := app.router.HandleFunc(path, f)
+	if internal {
+		route = route.Host("localhost")
+	}
+	return route
 }
 
 func (app *App) Start(port uint16) {
