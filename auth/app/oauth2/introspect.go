c088be5d643404e2f0fbcf4b3dc8c144e4a763e7
Allan Kerr <personal.allankerr@gmail.com>
2017-12-19 01:33:21 -0600

    Testing nginx replacement with ingress

--- auth/app/oauth2/introspect.go
+++ auth/app/oauth2/introspect.go
@@ -2,8 +2,32 @@ package oauth2
 
 import (
 	"net/http"
+	"github.com/sirupsen/logrus"
+	"strings"
+	"github.com/ory/fosite"
 )
 
 func (ctrl *HTTPController) Introspect(w http.ResponseWriter, req *http.Request) {
 
+	logger := logrus.WithFields(logrus.Fields{"endpoint": "Introspect"})
+
+	var scopes []string
+	scopesHeader := req.Header.Get("Scopes")
+	if scopesHeader == "" {
+		scopes = []string{}
+	} else {
+		scopes = strings.Split(scopesHeader, " ")
+	}
+
+	ar, err := ctrl.auth.IntrospectToken(req.Context(), fosite.AccessTokenFromRequest(req), fosite.AccessToken, nil, scopes...)
+	if err != nil {
+		logger.WithField("err", err).Info("request unauthorized")
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+
+	// TODO Write user and scope data to the response writer to be forwarded to the internal service
+	logger.Info("user: " + ar.GetID())
+
+	w.WriteHeader(http.StatusOK)
 }
