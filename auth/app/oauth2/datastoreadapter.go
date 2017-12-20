569c2087727982e896f4d0f152da9c1da6719ca5
Allan Kerr <personal.allankerr@gmail.com>
2017-12-20 02:49:22 -0600

    Updated sessions to have a time to live to automatically get removed upon expiry

--- auth/app/oauth2/datastoreadapter.go
+++ auth/app/oauth2/datastoreadapter.go
@@ -9,11 +9,14 @@ import (
 	"github.com/gocql/gocql"
 	"errors"
 	"github.com/sirupsen/logrus"
+	"time"
 )
 
 type DataStoreAdapter struct {
 	ds core.DataStore
 }
+
+const NO_TTL = time.Duration(0)
 
 func NewDatastoreAdapter(ds core.DataStore) *DataStoreAdapter {
 	adapter := new(DataStoreAdapter)
@@ -21,24 +24,48 @@ func NewDatastoreAdapter(ds core.DataStore) *DataStoreAdapter {
 	return adapter
 }
 
-func (adapter *DataStoreAdapter) createSession(sig string, req fosite.Requester) error {
+// createSession creates a new session record in the Cassandra database. To avoid revealing credentials
+// the form data is not persisted. The sig is the unique session identifier and the request consists
+// of the data to be persisted. To handle data that expires the ttl or time-to-live can be used. If the
+// data does not expire use NO_TTL. ttl must be non-negative.
+//
+func (adapter *DataStoreAdapter) createSession(sig string, req fosite.Requester, ttl time.Duration) error {
 
-
+	logrus.WithFields(logrus.Fields{
+		"signature":sig,
+		"ttl":ttl,
+	}).Debug("creating session")
 
 	session, ok := adapter.ds.GetSession().(*gocql.Session)
 	if !ok {
 		return errors.New("unexpected session type")
 	}
+
 	ses, err := NewSession(sig, req)
 	if err != nil {
 		return fosite.ErrServerError
 	}
+	if ttl < 0 {
+		logrus.Error("attempted to create session with invalid ttl: " + ttl.String())
+		return fosite.ErrServerError
+	}
 
-	stmt, names := qb.Insert("default.sessions").
-		Columns("signature", "request_id", "requested_at", "client_id", "scopes", "granted_scopes", "session_data").
-		ToCql()
+	cols := qb.Insert("default.sessions").
+		Columns("signature", "request_id", "requested_at", "client_id", "scopes", "granted_scopes", "session_data")
+	if ttl != NO_TTL {
+		cols = cols.TTL()
+	}
+	stmt, names := cols.ToCql()
 
-	q := gocqlx.Query(session.Query(stmt), names).BindStruct(ses)
+	q := gocqlx.Query(session.Query(stmt), names)
+	if ttl == NO_TTL {
+		q = q.BindStruct(ses)
+	} else {
+		q = q.BindStructMap(ses, qb.M{
+			"_ttl": qb.TTL(ttl),
+		})
+	}
+	logrus.Debug("query: " + q.String())
 	if err := q.ExecRelease(); err != nil {
 		logrus.WithField("error", err).Error("insert failed")
 		return fosite.ErrServerError
@@ -116,7 +143,7 @@ func (adapter *DataStoreAdapter) GetClient(_ context.Context, id string) (fosite
 func (adapter *DataStoreAdapter) CreateAuthorizeCodeSession(ctx context.Context, code string, req fosite.Requester) error {
 
 	logrus.Info("CreateAuthorizeCodeSession")
-	return adapter.createSession(code, req)
+	return adapter.createSession(code, req, NO_TTL)
 }
 
 func (adapter *DataStoreAdapter) GetAuthorizeCodeSession(ctx context.Context, code string, _ fosite.Session) (fosite.Requester, error) {
@@ -134,8 +161,9 @@ func (adapter *DataStoreAdapter) DeleteAuthorizeCodeSession(ctx context.Context,
 
 func (adapter *DataStoreAdapter) CreateAccessTokenSession(ctx context.Context, signature string, req fosite.Requester) error {
 
-	logrus.Info("CreateAccessTokenSession")
-	return adapter.createSession(signature, req)
+	logrus.Info("CreateAccessTokenSession: ")
+	ttl := req.GetSession().GetExpiresAt(fosite.AccessToken).Sub(time.Now().UTC())
+	return adapter.createSession(signature, req, ttl)
 }
 
 func (adapter *DataStoreAdapter) GetAccessTokenSession(ctx context.Context, signature string, _ fosite.Session) (fosite.Requester, error) {
@@ -154,7 +182,7 @@ func (adapter *DataStoreAdapter) DeleteAccessTokenSession(ctx context.Context, s
 func (adapter *DataStoreAdapter) CreateRefreshTokenSession(ctx context.Context, signature string, req fosite.Requester) error {
 
 	logrus.Info("CreateRefreshTokenSession")
-	return adapter.createSession(signature, req)
+	return adapter.createSession(signature, req, NO_TTL)
 }
 
 func (adapter *DataStoreAdapter) GetRefreshTokenSession(ctx context.Context, signature string, _ fosite.Session) (fosite.Requester, error) {
