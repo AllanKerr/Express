532dbc4c5dbeaf761dd9965670324b5d38f1a93d
Allan Kerr <personal.allankerr@gmail.com>
2017-12-29 01:25:22 -0600

    Added comment docs

--- gateway-controller/gateway-controller/kube/updater_deployment.go
+++ gateway-controller/gateway-controller/kube/updater_deployment.go
@@ -7,31 +7,39 @@ import (
 	"errors"
 )
 
+// The updater object for updating a deployment
+// Only the image may be updated
+// If image is nil then the old value is kept and the deployment is not updated.
 type ContainerUpdate struct {
 	Image *string
 }
 
+// The deployment update handler
 type DeploymentUpdater struct {
 	dInterface typedappsv1beta2.DeploymentInterface
 }
 
+// Create a new deployment updater to update a deployment on the specified namespace
 func NewDeploymentUpdater(client *Client, namespace string) *DeploymentUpdater {
 	return &DeploymentUpdater{
 		client.AppsV1beta2().Deployments(namespace),
 	}
 }
 
+// The list of modifiers that will result in a deployment update
 func (updater *DeploymentUpdater) GetModifiers() []string {
 	return []string{
 		"image",
 	}
 }
 
+// Performs an update on the existing deployment with the specified name
+// The update value must be a *ContainerUpdate
 func (updater *DeploymentUpdater) Update(name string, update interface{}) error {
 
 	containerUpdate, ok := update.(*ContainerUpdate)
 	if !ok {
-		return errors.New("Unexpected update type, expected *ContainerUpdate")
+		return errors.New("unexpected update type, expected *ContainerUpdate")
 	}
 	return retry.RetryOnConflict(retry.DefaultRetry, func() error {
 
