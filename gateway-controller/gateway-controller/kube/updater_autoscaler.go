c0eb90bf5028208b7bc0e8d70ff4047b9caf6292
Allan Kerr <personal.allankerr@gmail.com>
2017-12-29 00:50:08 -0600

    Added comments

--- gateway-controller/gateway-controller/kube/updater_autoscaler.go
+++ gateway-controller/gateway-controller/kube/updater_autoscaler.go
@@ -7,22 +7,27 @@ import (
 	"github.com/pkg/errors"
 )
 
+// Autoscaler update used to update an existing autoscaler
+// Only the min and max replicas can be updated
+// If either value is nil then that means the value should not be updated
 type AutoscalerUpdate struct {
 	MinReplicas *int32
 	MaxReplicas *int32
 }
 
-
+// The autoscaler update handler
 type AutoscalerUpdater struct {
 	aInterface typedautoscalingv2beta1.HorizontalPodAutoscalerInterface
 }
 
+// Create a new autoscaler updater to update an autoscaler on the specified namespace
 func NewAutoscalerUpdater(client *Client, namespace string) *AutoscalerUpdater {
 	return &AutoscalerUpdater{
 		client.AutoscalingV2beta1().HorizontalPodAutoscalers(namespace),
 	}
 }
 
+// The list of modifiers that will result in an autoscaler update
 func (updater *AutoscalerUpdater) GetModifiers() []string {
 	return []string {
 		"min",
@@ -30,6 +35,8 @@ func (updater *AutoscalerUpdater) GetModifiers() []string {
 	}
 }
 
+// Performs an update on the autoscaler with the specified name
+// The update value must be an *AutoscalerUpdate
 func (updater *AutoscalerUpdater) Update(name string, update interface{}) error {
 
 	autoscalerUpdate, ok := update.(*AutoscalerUpdate)
@@ -43,12 +50,15 @@ func (updater *AutoscalerUpdater) Update(name string, update interface{}) error 
 			return getErr
 		}
 
+		// Only update the values that were non-nil in the autoscaler update
 		if autoscalerUpdate.MinReplicas != nil {
 			result.Spec.MinReplicas = autoscalerUpdate.MinReplicas
 		}
 		if autoscalerUpdate.MaxReplicas != nil {
 			result.Spec.MaxReplicas = *autoscalerUpdate.MaxReplicas
 		}
+
+		// Check that min < max
 		if result.Spec.MinReplicas != nil && (result.Spec.MaxReplicas < *result.Spec.MinReplicas) {
 			return errors.New("max replicas must be greater than min replicas")
 		}
