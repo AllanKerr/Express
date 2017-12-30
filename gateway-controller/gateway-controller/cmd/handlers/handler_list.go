80780b2050473e2edab3e9ea5ba4e4ff3ed9190c
Allan Kerr <personal.allankerr@gmail.com>
2017-12-30 03:51:22 -0600

    Moved writer from printApplications to list

--- gateway-controller/gateway-controller/cmd/handlers/handler_list.go
+++ gateway-controller/gateway-controller/cmd/handlers/handler_list.go
@@ -7,20 +7,18 @@ import (
 	"os"
 	apiv1 "k8s.io/api/core/v1"
 	"gateway-controller/kube"
+	"io"
 )
 
 // Print a list of applications in a tab delimited table
-func printApplications(apps []kube.Application) {
-
-	w := new(tabwriter.Writer)
-	w.Init(os.Stdout, 0, 0, 4, ' ', 0)
+func printApplications(w io.Writer, apps []kube.Application) {
+	
 	fmt.Fprintln(w, "NAME\tPORT\tCREATION\t")
 	for _, app := range apps {
 		fmt.Fprintf(w, "%v\t", app.GetName())
 		fmt.Fprintf(w, "%v\t", app.GetPort())
 		fmt.Fprintf(w, "%v\t\n", app.GetCreationTimestamp())
 	}
-	w.Flush()
 }
 
 // List the set of deployed applications
@@ -30,5 +28,8 @@ func (ch *CommandHandler) List(command *cobra.Command, args []string) {
 	if err != nil {
 		fmt.Printf("Unable to list applications: %v\n", err)
 	}
-	printApplications(applications)
+	w := new(tabwriter.Writer)
+	w.Init(os.Stdout, 0, 0, 4, ' ', 0)
+	printApplications(w, applications)
+	w.Flush()
 }
