a699534fb2e2b8d786efea13b4198c058a1a2316
Allan Kerr <personal.allankerr@gmail.com>
2017-12-31 21:31:48 -0600

    Added client create and add scope tests

---
+++ containers/authorization/app/cmd/client_test.go
@@ -0,0 +1,36 @@
+package cmd
+
+import (
+	"testing"
+	"os"
+	"app/core"
+	"app/server"
+	"github.com/sirupsen/logrus"
+)
+
+func TestMain(m *testing.M) {
+
+	os.Chdir("..")
+	logrus.Info(os.Getwd())
+
+	databaseUrl := os.Getenv("DATABASE_URL")
+	ds := core.NewCQLDataStoreRetry(databaseUrl, "authorization", 1, 5)
+
+	if err := server.CreateSchema(ds); err != nil {
+		logrus.WithField("error", err).Error("Failed to create schema.")
+	}
+	retCode := m.Run()
+	ds.Close()
+	os.Exit(retCode)
+}
+
+// test creating a client and adding a scope to the client
+func TestClient_Client(t *testing.T) {
+
+	if err := createClient("test_client", "test_secret", false); err != nil {
+		t.Errorf("Error creating client: %v", err)
+	}
+	if err := addClientScope("test_client", "testscope"); err != nil {
+		t.Errorf("Error adding scope: %v", err)
+	}
+}
