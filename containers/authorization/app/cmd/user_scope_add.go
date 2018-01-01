3485ca1aca630aef952678c719bc25b9e1671f43
Allan Kerr <personal.allankerr@gmail.com>
2018-01-01 14:13:35 -0600

    Updated to use two functions to increase testibility

--- containers/authorization/app/cmd/user_scope_add.go
+++ containers/authorization/app/cmd/user_scope_add.go
@@ -6,32 +6,43 @@ import (
 	"app/oauth2"
 	"fmt"
 	"os"
+	"github.com/sirupsen/logrus"
 )
 
+// add a new scope to an existing user
+func addUserScope(username string, scope string) error {
+
+	// start a new CQL session
+	databaseUrl := os.Getenv("DATABASE_URL")
+	ds, err := core.NewCQLDataStore(databaseUrl, "default", 3)
+	if err != nil {
+		return err
+	}
+	defer ds.Close()
+
+	// get the user
+	adapter := oauth2.NewDataStoreAdapter(ds, config.GetHasher())
+	user, err := adapter.GetUser(nil, username)
+	if err != nil {
+		logrus.WithField("error", err).Info("Error getting user")
+		return err
+	}
+
+	// add scope and update
+	user.AppendScope(scope)
+	return adapter.UpdateUser(user, "scopes")
+}
+
+// cli command for adding scopes to users
 var cmdUserScopeAdd = &cobra.Command{
 	Use:   "add <username> <scope>",
 	Short: "Add a scope to an existing user",
 	Args: cobra.ExactArgs(2),
 	Run: func(cmd *cobra.Command, args []string) {
 
-		// start a new CQL session
-		databaseUrl := os.Getenv("DATABASE_URL")
-		ds, err := core.NewCQLDataStore(databaseUrl, "default", 3)
-		if err != nil {
-			fmt.Errorf("failed to create data store session %g", err)
-		}
-		defer ds.Close()
-
-		adapter := oauth2.NewDataStoreAdapter(ds, config.GetHasher())
-		user, err := adapter.GetUser(nil, args[0])
-		if err != nil {
-			fmt.Errorf("failed to get user: %g", err)
-		}
-		user.AppendScope(args[1])
-
-		err = adapter.UpdateUser(user, "scopes")
-		if err != nil {
-			fmt.Errorf("failed to update user: %g", err)
+		if err := addUserScope(args[0], args[1]); err != nil {
+			fmt.Errorf("failed to add scope %v", err)
+			return
 		}
 		fmt.Println("added scope: " + args[1])
 	},
