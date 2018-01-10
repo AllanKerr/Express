5986e393ade454f582262b7a29ad515962d08ee3
Allan Kerr <personal.allankerr@gmail.com>
2018-01-10 02:19:08 -0600

    Added list searches endpoint

--- integration-testing/locustfile.py
+++ integration-testing/locustfile.py
@@ -1,5 +1,9 @@
 from locust import HttpLocust, TaskSet
 import time
+import urllib3
+
+def get_path(path):
+    return "/testapi" + path
 
 def username():
     if 'num' not in username.__dict__:
@@ -14,20 +18,23 @@ def register(l):
         "confirm-password":"password"
     }, verify=False)
     if r.status_code == 200:
-        l.authorization = "Authorization: bearer " + r.json()["access_token"]
+        l.authorization = "bearer " + r.json()["access_token"]
         print(l.authorization)
 
-def test(l):
-    print("empty")
+def list_searches(l):
+    headers = {'Authorization': l.authorization}
+    path = get_path("/searches/v1/list")
+    l.client.get(path, headers=headers, verify=False)
 
 class UserBehavior(TaskSet):
-    tasks = {test: 1} #{profile: 1}
+    tasks = {list_searches: 1}
 
     def on_start(self):
         register(self)
-
 
 class WebsiteUser(HttpLocust):
     task_set = UserBehavior
     min_wait = 5000
     max_wait = 9000
+
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
