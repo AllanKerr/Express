46b1c33c63b3d9ed1bbd26f5de01d0af3ad18e5c
Allan Kerr <personal.allankerr@gmail.com>
2018-01-10 01:48:41 -0600

    Added registration at start of tests

--- integration-testing/locustfile.py
+++ integration-testing/locustfile.py
@@ -1,10 +1,31 @@
 from locust import HttpLocust, TaskSet
+import time
 
-def profile(l):
-    l.client.get("/test/", verify=False)
+def username():
+    if 'num' not in username.__dict__:
+        username.num = int(time.time())
+    username.num += 1
+    return "express-" + str(username.num)
+
+def register(l):
+    r = l.client.post("/oauth2/register", {
+        "username": username(),
+        "password":"password",
+        "confirm-password":"password"
+    }, verify=False)
+    if r.status_code == 200:
+        l.authorization = "Authorization: bearer " + r.json()["access_token"]
+        print(l.authorization)
+
+def test(l):
+    print("empty")
 
 class UserBehavior(TaskSet):
-    tasks = {profile: 1}
+    tasks = {test: 1} #{profile: 1}
+
+    def on_start(self):
+        register(self)
+
 
 class WebsiteUser(HttpLocust):
     task_set = UserBehavior
