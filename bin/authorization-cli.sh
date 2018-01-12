2ee3567d7de62fcb7f5aade80c366dc754f8268c
Allan Kerr <personal.allankerr@gmail.com>
2018-01-11 23:26:41 -0600

    Fixed bug causing failure with multiple pods

--- bin/authorization-cli.sh
+++ bin/authorization-cli.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo "$@"
-kubectl exec `kubectl get pod -l app=authorization | tail -n +2 | awk '$1 {print $1}'` -- app $@
+kubectl exec `kubectl get pod -l app=authorization | tail -n +2 | awk '$1 {print $1}' | head -1` -- app $@
